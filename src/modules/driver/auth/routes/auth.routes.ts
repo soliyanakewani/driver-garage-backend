28fc13ab603b981f208c3416be347301ba65a01f
Bethel Dereje <betheldereje5@gmail.com>
2026-04-13 11:56:17 +0300

    add rate limit for auth

--- src/modules/driver/auth/routes/auth.routes.ts
+++ src/modules/driver/auth/routes/auth.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { signup, login } from '../controllers/auth.controller';
 import { logout } from '../../../common/auth/logout.controller';
+import { loginLimiter } from '../../../../core/middleware/rate-limit.middleware';
 
 const router = Router();
 
 router.post('/signup', signup);
-router.post('/login', login);
+router.post('/login', loginLimiter, login);
 router.post('/logout', logout);
 
 export default router;
