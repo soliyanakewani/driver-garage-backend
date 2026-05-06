c0a44d8518d253b721aa6cd5e243df4abd971fb1
yordmela <yordanos.mela@gmail.com>
2026-05-06 20:23:27 +0300

    Add change password functionality for admin; update routes and service methods accordingly

--- src/modules/admin/auth/routes/auth.routes.ts
+++ src/modules/admin/auth/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
-import { login } from '../controllers/auth.controller';
+import { changePassword, login } from '../controllers/auth.controller';
 import { logout } from '../../../common/auth/logout.controller';
 import { loginLimiter } from '../../../../core/middleware/rate-limit.middleware';
+import { adminAuthGuard } from '../../../../core/middleware/auth/admin-auth.middleware';
 
 const router = Router();
 
 router.post("/login", loginLimiter, login);
 router.post('/logout', logout);
+router.put('/change-password', adminAuthGuard, changePassword);
 
 export default router;
